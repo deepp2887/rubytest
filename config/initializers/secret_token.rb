613a43fd81ebbf41857e9a29fc974701dc04a12d
deepp2887 <amrinder@anattadesign.com>
2013-09-13 17:24:41 +0530

    Created the test rails product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deeptest::Application.config.secret_key_base = '6894c17c80dba30582aff7f03924c6c0506587584c7b9daee5c788c3f8d69aa0746549d6c81ad303c62c7127736acbbeba81fd4be2d091adf62a099ee0e4f59f'
